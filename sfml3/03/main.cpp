07439dcd3e9ac66aa71fa049980a6b6909368e87
mr-ale141 <ale141@rambler.ru>
2023-09-28 15:22:53 +0300

    End sfml3/03 follow mose

--- sfml3/03/main.cpp
+++ sfml3/03/main.cpp
@@ -1,8 +1,30 @@
 // cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug .
 #include <SFML/Graphics.hpp>
+#include <cmath>
 #include <iostream>
 
-void pollEvents(sf::RenderWindow &window)
+void init(sf::ConvexShape& pointer)
+{
+    pointer.setPointCount(3);
+    pointer.setPoint(0, {40, 0});
+    pointer.setPoint(1, {-20, -20});
+    pointer.setPoint(2, {-20, 20});
+    pointer.setPosition({400, 300});
+    pointer.setFillColor(sf::Color(0xFF, 0x80, 0x00));
+}
+
+float toDegress(float radians)
+{
+    return float(double(radians) * 180.0 / M_PI);
+}
+
+void onMouseMove(const sf::Event::MouseMoveEvent& event, sf::Vector2f& mousePosition)
+{
+    std::cout << "mouse x=" << event.x << ", y=" << event.y << std::endl;
+    mousePosition = {float(event.x), float(event.y)};
+}
+
+void pollEvents(sf::RenderWindow &window, sf::Vector2f& mousePosition)
 {
     sf::Event event;
     while (window.pollEvent(event))
@@ -12,19 +34,8 @@ void pollEvents(sf::RenderWindow &window)
         case sf::Event::Closed:
             window.close();
             break;
-        case sf::Event::MouseButtonPressed:
-            std::cout
-                << "mouse pressed,"
-                << " x=" << event.mouseButton.x
-                << " y=" << event.mouseButton.y
-                << std::endl;
-            break;
-        case sf::Event::MouseButtonReleased:
-            std::cout
-                << "mouse released,"
-                << " x=" << event.mouseButton.x
-                << " y=" << event.mouseButton.y
-                << std::endl;
+        case sf::Event::MouseMoved:
+            onMouseMove(event.mouseMove, mousePosition);
             break;
         default:
             break;
@@ -32,9 +43,17 @@ void pollEvents(sf::RenderWindow &window)
     }
 }
 
-void redrawFrame(sf::RenderWindow &window)
+void update(const sf::Vector2f& mousePosition, sf::ConvexShape& pointer)
+{
+    const sf::Vector2f delta = mousePosition - pointer.getPosition();
+    const float angle = atan2(delta.y, delta.x);
+    pointer.setRotation(toDegress(angle));
+}
+
+void redrawFrame(sf::RenderWindow& window, sf::ConvexShape& pointer)
 {
     window.clear();
+    window.draw(pointer);
     window.display();
 }
 
@@ -43,13 +62,23 @@ int main()
     constexpr unsigned WINDOW_WIDTH = 800;
     constexpr unsigned WINDOW_HEIGHT = 600;
 
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
     sf::RenderWindow window(
         sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
-        "Prints mouse events to terminal");
+        "Pointer follows mouse",
+        sf::Style::Default,
+        settings);
+
+    sf::ConvexShape pointer;
+    sf::Vector2f mousePosition;
+
+    init(pointer);
 
     while (window.isOpen())
     {
-        pollEvents(window);
-        redrawFrame(window);
+        pollEvents(window, mousePosition);
+        update(mousePosition, pointer);
+        redrawFrame(window, pointer);
     }
 }
