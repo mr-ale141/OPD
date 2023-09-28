7eae74eb289a9bdea2c3583688f2cf29872816e8
mr-ale141 <ale141@rambler.ru>
2023-09-28 13:23:52 +0300

    End event mouse click

--- sfml3/02/main.cpp
+++ sfml3/02/main.cpp
@@ -1,27 +1,42 @@
 // cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug .
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    for (int num = 1; num <= 100; ++num)
+    constexpr unsigned WINDOW_WIDTH = 800;
+    constexpr unsigned WINDOW_HEIGHT = 600;
+
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Prints mouse events to terminal");
+
+    while (window.isOpen())
     {
-        switch (num % 15)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-        case 0:
-            std::cout << "FizzBuzz" << std::endl;
-            break;
-        case 5:
-        case 10:
-            std::cout << "Buzz" << std::endl;
-            break;
-        case 3:
-        case 6:
-        case 9:
-        case 12:
-            std::cout << "Fizz" << std::endl;
-            break;
-        default:
-            std::cout << num << std::endl;
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::MouseButtonPressed:
+                std::cout   << "mouse pressed,"
+                            << " x=" << event.mouseButton.x
+                            << " y=" << event.mouseButton.y
+                            << std::endl;
+                break;
+            case sf::Event::MouseButtonReleased:
+                std::cout   << "mouse released,"
+                            << " x=" << event.mouseButton.x
+                            << " y=" << event.mouseButton.y
+                            << std::endl;
+                break;
+            default:
+                break;
+            }
         }
+        
+        window.clear();
+        window.display();
     }
 }
