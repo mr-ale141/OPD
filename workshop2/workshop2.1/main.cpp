311c02e7a530c290352bead88c81602aac30551b
mr-ale141 <ale141@rambler.ru>
2023-10-19 10:57:53 +0300

    Workshop2.1 end

--- workshop2/workshop2.1/main.cpp
+++ workshop2/workshop2.1/main.cpp
@@ -1,7 +1,20 @@
 // cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -S ./workshop1.1/ -B ./workshop1.1/
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <vector>
+
+constexpr unsigned WINDOW_WIDTH = 800;
+constexpr unsigned WINDOW_HEIGHT = 600;
+constexpr unsigned BALL_SIZE = 50;
+
+struct circleStruct
+{
+    sf::CircleShape circle;
+    sf::Vector2f speed;
+    float preTime;
+};
 
 sf::Vector2f toEuclidean(float radius, float angle)
 {
@@ -16,12 +29,7 @@ float toDegress(float radians)
     return float(double(radians) * 180.0 / M_PI);
 }
 
-void onMouseClick(const sf::Event::MouseButtonEvent& event, sf::Vector2f& mousePositionClick)
-{
-    mousePositionClick = { float(event.x), float(event.y) };
-}
-
-void pollEvents(sf::RenderWindow& window, sf::Vector2f& mousePositionClick, bool& caught)
+void pollEvents(sf::RenderWindow& window)
 {
     sf::Event event;
     while (window.pollEvent(event))
@@ -31,89 +39,45 @@ void pollEvents(sf::RenderWindow& window, sf::Vector2f& mousePositionClick, bool
         case sf::Event::Closed:
             window.close();
             break;
-        case sf::Event::MouseButtonPressed:
-            caught = false;
-            onMouseClick(event.mouseButton, mousePositionClick);
-            break;
         default:
             break;
         }
     }
 }
 
-void updateAngleСat(float preTime[2], const sf::Vector2f& mousePositionClick, sf::Sprite& cat, sf::Clock& clock)
+void update(std::vector<circleStruct>& circles, sf::Clock& clock)
 {
-    if ((mousePositionClick.x - cat.getPosition().x) < 0)
-        cat.setScale(-1, 1);
-    else
-        cat.setScale(1, 1);
-}
-
-void updatePositionСat(
-    bool& caught,
-    float preTime[2],
-    const sf::Vector2f& mousePositionClick,
-    sf::Sprite& cat,
-    sf::Clock& clock)
-{
-    constexpr float speed = 20.f;
-    const float time = clock.getElapsedTime().asSeconds();
-    const float dTime = time - preTime[0];
-    preTime[0] = time;
-    const sf::Vector2f catPos = cat.getPosition();
-    const sf::Vector2f offset = mousePositionClick - catPos;
-    if (offset.x != 0 && offset.y != 0)
+    for (int i = 0; i < std::size(circles); ++i)
     {
-        const float dist = std::abs(std::sqrt(offset.x * offset.x + offset.y * offset.y));
-        const sf::Vector2f normOffset = { offset.x / dist, offset.y / dist };
-        const float stepLen = speed * dTime;
-        const sf::Vector2f mov = { normOffset.x * stepLen, normOffset.y * stepLen };
-        cat.setPosition(catPos + mov);
-    }
-    else
-        caught = true;
-}
-
-void update(
-    bool& caught,
-    float preTime[2],
-    const sf::Vector2f& mousePositionClick,
-    sf::Sprite& cat,
-    sf::Sprite& redPointer,
-    sf::Clock& clock)
-{
-    redPointer.setPosition(mousePositionClick);
-    if (caught == false)
-    {
-        updateAngleСat(preTime, mousePositionClick, cat, clock);
-        updatePositionСat(caught, preTime, mousePositionClick, cat, clock);
-    }
-    else
-    {
-        preTime[0] = clock.getElapsedTime().asSeconds();
-        preTime[1] = clock.getElapsedTime().asSeconds();
+        float currTime = clock.getElapsedTime().asSeconds();
+        float dt = currTime - circles[i].preTime;
+        sf::Vector2f position = circles[i].circle.getPosition();
+        position += circles[i].speed * dt;
+        if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) && (circles[i].speed.x > 0))
+            circles[i].speed.x = -circles[i].speed.x;
+        if ((position.x < 0) && (circles[i].speed.x < 0))
+            circles[i].speed.x = -circles[i].speed.x;
+        if ((position.y + 2 * BALL_SIZE >= WINDOW_HEIGHT) && (circles[i].speed.y > 0))
+            circles[i].speed.y = -circles[i].speed.y;
+        if ((position.y < 0) && (circles[i].speed.y < 0))
+            circles[i].speed.y = -circles[i].speed.y;
+        circles[i].circle.setPosition(position);
+        circles[i].preTime = currTime;
     }
 }
 
-void redrawFrame(sf::RenderWindow& window, sf::Sprite& cat, sf::Sprite& redPint, bool caught)
+void redrawFrame(sf::RenderWindow& window, std::vector<circleStruct>& circles)
 {
     window.clear(sf::Color(0xFF, 0xFF, 0xFF));
-    window.draw(cat);
-    if (caught == false)
-        window.draw(redPint);
+    for (circleStruct circleItem : circles)
+    {
+        window.draw(circleItem.circle);
+    }
     window.display();
 }
 
 int main()
 {
-    constexpr unsigned WINDOW_WIDTH = 800;
-    constexpr unsigned WINDOW_HEIGHT = 600;
-
-    float preTime[2];
-
-    sf::Clock clock;
-    preTime[0] = clock.getElapsedTime().asSeconds();
-    preTime[1] = clock.getElapsedTime().asSeconds();
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
@@ -123,31 +87,36 @@ int main()
         sf::Style::Default,
         settings);
 
-    sf::Texture textureCat;
-    if (!textureCat.loadFromFile("cat.png"))
-    {
-        return 1;
-    }
-    sf::Texture textureRedPoint;
-    if (!textureRedPoint.loadFromFile("red_pointer.png"))
-    {
-        return 1;
-    }
+    sf::CircleShape circleRed(BALL_SIZE);
+    circleRed.setPosition({ 200, 120 });
+    circleRed.setFillColor(sf::Color(0xFF, 0x00, 0x00));
+    sf::CircleShape circleYellow(BALL_SIZE);
+    circleYellow.setPosition({ 150, 200 });
+    circleYellow.setFillColor(sf::Color(0xFF, 0xFF, 0x00));
+    sf::CircleShape circleBlue(BALL_SIZE);
+    circleBlue.setPosition({ 400, 450 });
+    circleBlue.setFillColor(sf::Color(0x00, 0x00, 0xFF));
+    sf::CircleShape circleGreen(BALL_SIZE);
+    circleGreen.setPosition({ 350, 480 });
+    circleGreen.setFillColor(sf::Color(0x00, 0xFF, 0x00));
+    sf::CircleShape circleBlack(BALL_SIZE);
+    circleBlack.setPosition({ 100, 50 });
+    circleBlack.setFillColor(sf::Color(0x00, 0x00, 0x00));
 
-    sf::Sprite cat;
-    cat.setTexture(textureCat);
-    cat.setOrigin({ 19.f, 12.f });
-    cat.setPosition({ 400, 300 });
-    sf::Sprite redPoint;
-    redPoint.setTexture(textureRedPoint);
-    redPoint.setOrigin({ 16.f, 16.f });
-    bool caught = true;
-    sf::Vector2f mousePositionClick;
+    sf::Clock clock;
+
+    std::vector<circleStruct> circles = {
+        { circleRed, { 50.f, 75.f }, clock.getElapsedTime().asSeconds() },
+        { circleYellow, { -50.f, -75.f }, clock.getElapsedTime().asSeconds() },
+        { circleBlue, { 75.f, 50.f }, clock.getElapsedTime().asSeconds() },
+        { circleGreen, { -75.f, -50.f }, clock.getElapsedTime().asSeconds() },
+        { circleBlack, { 80.f, -60.f }, clock.getElapsedTime().asSeconds() }
+    };
 
     while (window.isOpen())
     {
-        pollEvents(window, mousePositionClick, caught);
-        update(caught, preTime, mousePositionClick, cat, redPoint, clock);
-        redrawFrame(window, cat, redPoint, caught);
+        pollEvents(window);
+        update(circles, clock);
+        redrawFrame(window, circles);
     }
 }
